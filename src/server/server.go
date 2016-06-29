64a51ebae75b78b4e26b9e92a4b67edf8adfca45
Mark Lee <markleechuyong@gmail.com>
2016-06-29 13:22:45 +0800

    small changes to server.go

--- src/server/server.go
+++ src/server/server.go
@@ -3,10 +3,10 @@ package main
 import "github.com/go-martini/martini"
 
 func main() {
-    server := martini.Classic()
-    server.Get("/", func() string {
-        return "<h1>Hello, world!</h1>"
-    })
+	server := martini.Classic()
+	server.Get("/", func() string {
+		return "<h1>Hello, world!</h1>"
+	})
 
-    server.Run()
+	server.Run()
 }
